36bac1e825072e3c404846978ceef3bd46729d86
Paul Wlodkowski <pawlodkowski@gmail.com>
2021-10-29 12:56:13 +0200

    modified setup script

--- setup.py
+++ setup.py
@@ -1,26 +1,29 @@
+import os
+import sys
+
 from setuptools import setup
 from setuptools.command.develop import develop
-import os
+
+PREFIX_TARGETS = [
+    ("nbconvert/templates", 'flowkey')
+]
+
 try:
     import jupyter_core.paths as jupyter_core_paths
-except:
+except ModuleNotFoundError:
     jupyter_core_paths = None
 
 
-pjoin = os.path.join
-
-
 class DevelopCmd(develop):
-    prefix_targets = [
-        ("nbconvert/templates", 'acme')
-    ]
+
     def run(self):
         target_dir = os.path.join(sys.prefix, 'share', 'jupyter')
-        if '--user' in sys.prefix:  # TODO: is there a better way to find out?
-            target_dir = jupyter_core_paths.user_dir()
+        if jupyter_core_paths:
+            #TODO: potentially brittle logic: see https://jupyter.readthedocs.io/en/latest/use/jupyter-directories.html#envvar-JUPYTER_PATH
+            target_dir = jupyter_core_paths.jupyter_path()[1]
         target_dir = os.path.join(target_dir)
 
-        for prefix_target, name in self.prefix_targets:
+        for prefix_target, name in PREFIX_TARGETS:
             source = os.path.join('share', 'jupyter', prefix_target, name)
             target = os.path.join(target_dir, prefix_target, name)
             target_subdir = os.path.dirname(target)
@@ -31,29 +34,32 @@ class DevelopCmd(develop):
                 os.remove(target)
             except:
                 pass
+            #TODO: is this symbolic link needed?
             print(rel_source, '->', target)
             os.symlink(rel_source, target)
 
-        super(DevelopCmd, self).run()
+        super().run()
 
-
+###TODO Later: add relative paths of data files to data_files value of setup args
 data_files = []
-for root, dirs, files in os.walk('share'):
-    root_files = [os.path.join(root, i) for i in files]
-    data_files.append((root, root_files))
+# for prefix_target, name in PREFIX_TARGETS:
+#     source = os.path.join('share', 'jupyter', prefix_target, name)
+#     for root, dirs, files in os.walk(source):
+#         root_files = [os.path.join(root, i) for i in files]
+#         data_files.append((root, root_files))
 
 setup_args = {
-    'name': 'nbconvert-acme',
+    'name': 'nbconvert-flowkey',
     'version': '0.1.0',
     'packages': [],
     'data_files': data_files,
     'install_requires': [
     ],
-    'author': 'Sylvain Corlay',
-    'author_email': 'jupyter@googlegroups.com',
-    'url': 'https://github.com/SylvainCorlay/nbconvert-acme/',
+    'author': 'Paul Wlodkowski',
+    'author_email': 'paul@flowkey.com',
+    'url': 'https://github.com/pawlodkowski/nbconvert-flowkey',
     'cmdclass': {
-        'develop': DevelopCmd,
+        'develop': DevelopCmd, #https://stackoverflow.com/a/27820612/17242197
     } if jupyter_core_paths else {},
 }
 
